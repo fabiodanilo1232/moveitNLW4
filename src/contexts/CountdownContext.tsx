22f331b78cd14d094aad8247d042e8dc0ff0e40c
fabiodanilo1232 <fabiodanilo720@gmail.com>
2021-02-28 15:17:29 -0300

    Ajuste do tempo do countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps){
     const { startNewChallenge } = useContext(ChallengesContext)
 
-    const [time, setTime] = useState(0.05*60);
+    const [time, setTime] = useState(25*60);
     const [isActive, setisActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -35,7 +35,7 @@ export function CountdownProvider({ children }: CountdownProviderProps){
     function resetCountdown(){
         clearTimeout(countdownTimeout);
         setisActive(false);
-        setTime(0.05 * 60);
+        setTime(25 * 60);
         setHasFinished(false);
     }
     
